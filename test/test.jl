ca3cc863bc11684ba5d2eef30443c95fee8c0830
Erik Engheim <erik.engheim@mac.com>
2014-12-31 00:58:00 +0100

    tests for plist lexer

---
+++ test/test.jl
@@ -0,0 +1,32 @@
+using PikeLexer
+using Base.Test
+
+l = Lexer(open(readall, "test/example.plist"))
+
+@test lex_plist(l) == Token(LBRACE)
+@test lex_plist(l) == Token(IDENT, "Dogs")
+@test lex_plist(l) == Token(EQUAL)
+@test lex_plist(l) == Token(LPAREN)
+@test lex_plist(l) == Token(LBRACE)
+@test lex_plist(l) == Token(IDENT, "Name")
+@test lex_plist(l) == Token(EQUAL)
+@test lex_plist(l) == Token(STRING, "Scooby Doo")
+@test lex_plist(l) == Token(SEMICOLON)
+@test lex_plist(l) == Token(IDENT, "Age")
+@test lex_plist(l) == Token(EQUAL)
+@test lex_plist(l) == Token(NUMBER, "43")
+@test lex_plist(l) == Token(SEMICOLON)
+@test lex_plist(l) == Token(IDENT, "Colors")
+@test lex_plist(l) == Token(EQUAL)
+@test lex_plist(l) == Token(LPAREN)
+@test lex_plist(l) == Token(IDENT, "Brown")
+@test lex_plist(l) == Token(COMMA)
+@test lex_plist(l) == Token(IDENT, "Black")
+@test lex_plist(l) == Token(RPAREN)
+@test lex_plist(l) == Token(SEMICOLON)
+@test lex_plist(l) == Token(RBRACE)
+@test lex_plist(l) == Token(RPAREN)
+@test lex_plist(l) == Token(SEMICOLON)
+@test lex_plist(l) == Token(RBRACE)
+
+
